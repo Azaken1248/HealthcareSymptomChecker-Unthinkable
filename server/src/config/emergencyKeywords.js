2d4448578d3e1e6b3fa6372afae68dc1bad69a53
Azaken1248 <suvkrm@gmail.com>
2025-10-12 15:29:04 +0530

    Added Emergency Keywords List

---
+++ server/src/config/emergencyKeywords.js
@@ -0,0 +1,27 @@
+// File: src/config/emergencyKeywords.js
+
+// This list contains keywords that will trigger the "criticalWarning" flag.
+// The check will be case-insensitive.
+export const emergencyKeywords = [
+  "chest pain",
+  "crushing pain",
+  "shortness of breath",
+  "can't breathe",
+  "cannot breathe",
+  "difficulty breathing",
+  "numbness in face",
+  "numbness in arm",
+  "slurred speech",
+  "sudden vision loss",
+  "blurry vision",
+  "severe headache",
+  "worst headache",
+  "suicidal",
+  "harm myself",
+  "unconscious",
+  "not breathing",
+  "seizure",
+  "convulsion",
+  "choking",
+  "severe bleeding",
+];
